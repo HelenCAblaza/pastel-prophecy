05535d1720af6c1b506e8800c732304dd8ae8b20
Helen C. Ablaza <284748959+HelenCAblaza@users.noreply.github.com>
2026-06-16 13:27:32 +0000

    Add Queen of Cups card art

--- data/cards.js
+++ data/cards.js
@@ -452,7 +452,7 @@ export const cards = [
     "heartMeaning": "Your heart is touching nurture. Queen of Dewdrops asks you to be gentle with what you feel before you try to fix it.",
     "pathMeaning": "Your path is moving through nurture. Let Queen of Dewdrops show you the next small step instead of the whole staircase.",
     "magicMeaning": "Your hidden magic is nurture. Queen of Dewdrops brings a soft blessing that helps the day open in your favor.",
-    "artPath": "assets/cards/dewdrops/queen-of-dewdrops.svg"
+    "artPath": "assets/cards/imported/queen-of-cups.jpg"
   },
   {
     "id": "king-of-dewdrops",
